112c48ad03bc1aa2965ec8baa78d4bad2adbce51
Kye Purkis <kye44@hotmail.co.uk>
2018-07-30 02:41:13 +0100

    Minor tweaks.

--- bot.py
+++ bot.py
@@ -205,4 +205,4 @@ def CheckServerForAccounts():
 		if CheckAccount(user.name) == False:
 			a = Account()
 			a.Create(user.name)
-client.run('NDY5MTcwOTk3NTAxNjI0MzMw.DjD1IA.NtakvdszP945tmDTrSLXSJSFrsM')
+client.run('')
